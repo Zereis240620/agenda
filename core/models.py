e4e059f37ccdaa551442ec2f2cd72fda9996f5ab
zereis240620 <josedrm2000@gmail.com>
2021-10-23 22:58:36 -0300

    Criado tela de listagem de eventos

--- core/models.py
+++ core/models.py
@@ -14,7 +14,10 @@ class Evento(models.Model):
         db_table = 'evento'
 
     def __str__(self):
-        return self.titulo
+        return self.descricao
+
+    def get_data_event(self):
+        return self.data_evento.strftime('%d/%m/%Y %H:%M Hrs')
 
 
 
--- core/views.py
+++ core/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, HttpResponse, redirect
+from core.models import Evento
 # Create your views here.
+
+#
+# def index(request):
+#     return redirect("/agenda")
+
+def listaEvento(request):
+    evento = Evento.objects.all()
+    data = {'eventos':evento}
+    return render(request, 'agenda.html', data)
+
+def eventoLocal(request,titulo_evento):
+    data = Evento.objects.filter(titulo = titulo_evento)
+
+    if(len(data) <= 0):
+        return HttpResponse('Nenhum Evento Encontrado')
+    else:
+        return HttpResponse(data[0])
