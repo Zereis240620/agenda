f8406ba756f3cd5f9423d7782d4d8230f9afec09
zereis240620 <josedrm2000@gmail.com>
2021-10-24 00:31:31 -0300

    Criação de campo local no Model de Evento, e adição do mesmo no template de evento

--- core/models.py
+++ core/models.py
@@ -1,14 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
 # Create your models here.
 class Evento(models.Model):
     titulo = models.CharField(max_length=100)
     descricao = models.TextField(blank=True, null=True)
     data_evento = models.DateTimeField(verbose_name='Data do Evento')
     data_criacao = models.DateTimeField(auto_now=True)
-    local = models.TextField(max_length=100, blank=True, null=True)
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
 
     # definindo nome da tabela ao executar migrations, caso não defina ele pega o nome do app e concatena com a classe
@@ -20,3 +18,6 @@ class Evento(models.Model):
 
     def get_data_event(self):
         return self.data_evento.strftime('%d/%m/%Y %H:%M Hrs')
+
+
+
