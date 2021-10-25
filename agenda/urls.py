9a740060f0b46e71abdc971fbd52895dbd0cfa54
zereis240620 <josedrm2000@gmail.com>
2021-10-25 11:59:25 -0300

    Adicionado funções de alteração e exclusão de dados

--- agenda/urls.py
+++ agenda/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('agenda/', views.listaEvento),
     path('agenda/evento/', views.evento),
     path('agenda/evento/submit', views.submit_evento),
+    path('agenda/evento/delete/<int:id_evento>/', views.delete_evento),
 
     #path('', views.index),
     path('', RedirectView.as_view(url="/agenda/")),
--- core/views.py
+++ core/views.py
@@ -38,7 +38,12 @@ def listaEvento(request):
 
 @login_required(login_url="/login/")
 def evento(request):
-    return render(request, 'evento.html')
+    id_evento = request.GET.get('id')
+    dados = {}
+    if(id_evento):
+        dados['evento'] = Evento.objects.get(id=id_evento)
+
+    return render(request, 'evento.html', dados)
 
 @login_required(login_url="/login/")
 def submit_evento(request):
@@ -47,9 +52,29 @@ def submit_evento(request):
         data_evento = request.POST.get('data_evento')
         descricao   = request.POST.get('descricao')
         usuario     = request.user
+        id_evento   = request.POST.get('id_evento')
 
-        Evento.objects.create(titulo=titulo, data_evento=data_evento, descricao=descricao, usuario=usuario)
+        if(id_evento):
+            evento = Evento.objects.get(id=id_evento)
+            if(evento.usuario == usuario):
+                evento.titulo = titulo
+                evento.data_evento = data_evento
+                evento.descricao = descricao
+                evento.save()
 
+            # Evento.objects.filter(id=id_evento).update(titulo=titulo, data_evento=data_evento, descricao=descricao)
+        else:
+            Evento.objects.create(titulo=titulo, data_evento=data_evento, descricao=descricao, usuario=usuario)
+
+
+    return redirect('/')
+
+@login_required(login_url="/login/")
+def delete_evento(request, id_evento):
+    usuario = request.user
+    evento = Evento.objects.get(id=id_evento)
+    if(usuario == evento.usuario):
+        evento.delete()
 
     return redirect('/')
 
