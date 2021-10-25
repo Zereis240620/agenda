d33d976bbfa663e8114cddad9d3579fd7b608984
zereis240620 <josedrm2000@gmail.com>
2021-10-25 15:28:04 -0300

    Adicionado retorna de lista por json, retorno de http404 e configurado timezone para pt-br

--- agenda/settings.py
+++ agenda/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-hc@r&3!^+xer_%3@pe7u+grp#$@g*%m4x!d1%&1()ml&7)(kv-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -104,15 +104,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
--- core/views.py
+++ core/views.py
@@ -1,8 +1,11 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render, HttpResponse, redirect
 from core.models import Evento
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import  authenticate, login, logout
 from django.contrib import messages
+from datetime import  datetime, timedelta
+from django.http.response import Http404, JsonResponse
 # Create your views here.
 
 #
@@ -31,7 +34,9 @@ def submit_login(request):
 @login_required(login_url="/login/")
 def listaEvento(request):
     usuario = request.user
-    evento = Evento.objects.filter(usuario=usuario)
+    data_atual = datetime.now() - timedelta(hours=1)
+    # filtro datas __gt maior e __lt menor
+    evento = Evento.objects.filter(usuario=usuario, data_evento__lt= data_atual)
     data = {'eventos':evento}
     return render(request, 'agenda.html', data)
 
@@ -72,16 +77,28 @@ def submit_evento(request):
 @login_required(login_url="/login/")
 def delete_evento(request, id_evento):
     usuario = request.user
-    evento = Evento.objects.get(id=id_evento)
+    try:
+        evento = Evento.objects.get(id=id_evento)
+    except Exception:
+        raise Http404()
+
     if(usuario == evento.usuario):
         evento.delete()
+    else:
+        raise Http404()
 
     return redirect('/')
 
+@login_required(login_url="/login/")
+def json_lista_evento(request,id_usuario):
+    usuario = User.objects.get(id=id_usuario)
+    evento = Evento.objects.filter(usuario=usuario).values('id', 'titulo')
+
+    return JsonResponse(list(evento), safe=False)
 
 @login_required(login_url="/login/")
 def eventoLocal(request,titulo_evento):
-    data = Evento.objects.filter(titulo = titulo_evento)
+    data = Evento.objects.filter(titulo=titulo_evento)
 
     if(len(data) <= 0):
         return HttpResponse('Nenhum Evento Encontrado')
